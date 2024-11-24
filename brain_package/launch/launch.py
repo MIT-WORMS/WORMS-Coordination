892dadf8c53df5d9bd6b1c49c201ed91c1e04f42
WORMS-desktop <WORMS-desktop@mit.edu>
2024-11-24 17:19:01 -0500

    Looped in controller_package.

--- brain_package/launch/launch.py
+++ brain_package/launch/launch.py
@@ -2,6 +2,10 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 import os
 import pandas as pd
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 
 def generate_launch_description():
     WORKSPACE_NAME = "WORMS-software-ws"
@@ -11,7 +15,7 @@ def generate_launch_description():
     working_file_path = os.path.dirname(os.path.realpath(__file__))
     end_index = working_file_path.find(WORKSPACE_NAME) + len(WORKSPACE_NAME)
     script_directory = os.path.join(working_file_path[:end_index], "src", REPO_NAME, PACKAGE_NAME, PACKAGE_NAME)
-    command_filepath = os.path.join(script_directory, "run_stand_forward_gait.csv")
+    command_filepath = os.path.join(script_directory, "all_worms.csv")
     df = pd.read_csv(command_filepath)
     all_worms = df.columns
 
@@ -21,11 +25,16 @@ def generate_launch_description():
         node = Node(package=PACKAGE_NAME,
                     executable="brain",
                     namespace=worm,
-                    name='brain',
+                    name='brain', # TODO: remove this line
                     remappings=[
                         (f'/{worm}/personal_communication_topic', '/personal_communication_topic'),
                         (f'/{worm}/system_communication_topic', '/system_communication_topic'),
                         (f'/{worm}/readiness_communication_topic', '/readiness_communication_topic')
                     ])
         all_nodes.append(node)
+    controller_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('controller_package'), 'launch', 'launch.py')])
+    )
+    all_nodes.append(controller_launch)
     return LaunchDescription(all_nodes)
